8ab00e0f8ba8ebd462806dea48f58fcf1de48d32
Thymmz <atharva.deshpande228@gmail.com>
2022-02-15 18:06:24 -0500

    added to search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,21 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-   const products = await Product.find({featured : true})
+   const products = await Product.find({name : 'albany'})
    res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req, res) => {
-   const {featured} = req.query
+   const {featured, company, name} = req.query
    const queryObject = {}
    if (featured){
       queryObject.featured = featured==='true'? true : false
+   }
+   if(company){
+      queryObject.company = company
+   }
+   if(name){
+      queryObject.name = name
    }
    const products = await Product.find(queryObject)
    res.status(200).json({products, nbHits:products.length})
