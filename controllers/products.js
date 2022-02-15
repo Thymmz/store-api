5f2a9572bf797f5ad2eb7f1bdc645beb21f49d27
Thymmz <atharva.deshpande228@gmail.com>
2022-02-15 12:55:32 -0500

    basic search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,18 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req,res) => {
-   throw new Error('Testing async errors')
-   res.status(200).json({msg:'products testing route'})
+   const products = await Product.find({featured : true})
+   res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req, res) => {
-   res.status(200).json({msg:'products route'})
+   const {featured} = req.query
+   const queryObject = {}
+   if (featured){
+      queryObject.featured = featured==='true'? true : false
+   }
+   const products = await Product.find(queryObject)
+   res.status(200).json({products, nbHits:products.length})
 }
 
 module.exports = {
